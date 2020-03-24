17c9d4ff870085189aea501a493a4f9ceef9e490
szantilas87 <szantilas@gmail.com>
2020-03-24 08:31:14 +0000

    Card class

--- black_jack.py
+++ black_jack.py
@@ -5,3 +5,19 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
           'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
+
+class Card():
+    """
+    Creates new card object
+    """
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        for k, v in values.items():
+            if self.rank == k:
+                self.value = v
+
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
